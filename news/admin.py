1dc8123675c8923e4ced0d938ae0a8bed6f967d4
Jasmina <jasminamusuronqulova9@gmail.com>
2025-03-10 16:25:43 +0500

    done

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import New
 
-# Register your models here.
+
+@admin.register(New)
+class NewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'views_count', 'category']
+    search_fields = ['title', 'content', 'tags']
+    prepopulated_fields = {"slug": ("title",)}
