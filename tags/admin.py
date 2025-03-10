1cf13677314058ddb3075d80eefc2c3934b791b9
Jasmina <jasminamusuronqulova9@gmail.com>
2025-03-10 21:43:25 +0500

    done

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tag
 
-# Register your models here.
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+    prepopulated_fields = {"slug": ("name",)}
